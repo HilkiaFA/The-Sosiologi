09d3839094df358ff1c71b15ffdd9cf6c4dbd46a
HilkiaFA <hilkiafarrel13@gmail.com>
2023-08-31 11:35:23 +0700

    Game Sosiologi

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let wrongAnswers = 0;
 
 function checkAnswer(answer) {
   if (answer === 'a') {
-    window.location.href = `index.html?wrongAnswers=4`;
+    window.location.href = `game2.html?wrongAnswers=4`;
 }else if (answer === 'b') {
-    window.location.href = `index.html?wrongAnswers=1`;
+    window.location.href = `game2.html?wrongAnswers=1`;
 }
 else if (answer === 'c') {
-    window.location.href = `index.html?wrongAnswers=2`;
+    window.location.href = `game2.html?wrongAnswers=2`;
 }
 else if (answer === 'd') {
-    window.location.href = `index.html?wrongAnswers=5`;
+    window.location.href = `game2.html?wrongAnswers=5`;
 }
 }
---
+++ script2.js
@@ -0,0 +1,37 @@
+
+let wrongAnswers = 0;
+let wrongAnswers1 = 0;
+let totalWrongAnswers = 0;
+
+function checkAnswer(answer) {
+  if (answer === 'a') {
+    const urlParams = new URLSearchParams(window.location.search);
+    wrongAnswers = parseInt(urlParams.get('wrongAnswers'));
+    totalWrongAnswers = wrongAnswers + 4;
+    window.location.href = `game3.html?wrongAnswers=${totalWrongAnswers}`;
+  }
+  else if (answer === 'b') {
+    const urlParams = new URLSearchParams(window.location.search);
+    wrongAnswers = parseInt(urlParams.get('wrongAnswers'));
+    totalWrongAnswers = wrongAnswers + 1;
+    window.location.href = `index.html?wrongAnswers=${totalWrongAnswers}`;
+  }
+  else if (answer === 'c') {
+    const urlParams = new URLSearchParams(window.location.search);
+    wrongAnswers = parseInt(urlParams.get('wrongAnswers'));
+    totalWrongAnswers = wrongAnswers + 3;
+    window.location.href = `index.html?wrongAnswers=${totalWrongAnswers}`;
+  }
+  else if (answer === 'd') {
+    const urlParams = new URLSearchParams(window.location.search);
+    wrongAnswers = parseInt(urlParams.get('wrongAnswers'));
+    totalWrongAnswers = wrongAnswers + 5;
+    window.location.href = `index.html?wrongAnswers=${totalWrongAnswers}`;
+  }
+  else if (answer === 'e') {
+    const urlParams = new URLSearchParams(window.location.search);
+    wrongAnswers = parseInt(urlParams.get('wrongAnswers'));
+    totalWrongAnswers = wrongAnswers + 2;
+    window.location.href = `index.html?wrongAnswers=${totalWrongAnswers}`;
+  }
+}
